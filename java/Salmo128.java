911f5afa0bf16b6d300d6bf8d7b7ea6a081572ca
David Montenegro <davidom.official@gmail.com>
2023-02-17 10:03:16 -0300

    add salmo128

---
+++ java/Salmo128.java
@@ -0,0 +1,31 @@
+public class Salmo128 {
+
+	public static void main(String[] args) {
+		System.out.println("Bem-aventurado_aquele");
+		System.out.println("que temor tem ao Senhor");
+		System.out.println("E que anda nos caminhos,");
+		System.out.println("os caminhos de Javé");
+		System.out.println("Do labor das tuas mãos");
+		System.out.println("o teu pão granjearás");
+		System.out.println("E feliz, então, serás");
+		System.out.println("e o bem te seguirá.");
+		System.out.println("");
+		System.out.println("Tua_esposa,_em tua casa,");
+		System.out.println("é videira_a florescer");
+		System.out.println("Quais rebentos de_oliveira");
+		System.out.println("os teus filhos brotarão");
+		System.out.println("Ao redor da tua mesa,");
+		System.out.println("todos juntos estarão");
+		System.out.println("Eis a bênção que alcança");
+		System.out.println("quem assim teme_ao SENHOR.");
+		System.out.println("");
+		System.out.println("Que o Senhor te abençoe");
+		System.out.println("desde_o monte de Sião");
+		System.out.println("E,_em Jerusalém, tu vejas");
+		System.out.println("todo_o bem que haverá");
+		System.out.println("Pelo tempo dos teus dias,");
+		System.out.println("pelos anos do viver");
+		System.out.println("Filhos dos teus filhos vejas.");
+		System.out.println("Seja_a paz sobre_Israel!");
+	}
+}
