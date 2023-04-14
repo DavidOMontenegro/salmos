a5d6605485bfb79dd492dff0cad013a0b41ce15c
David Montenegro <davidom.official@gmail.com>
2023-04-14 10:16:55 -0300

    fix salmo137

--- java/Salmo137.java
+++ java/Salmo137.java
@@ -1,24 +1,24 @@
 public class Salmo137 {
 
 	public static void main(String[] args) {
-		System.out.println("Nas margens lá dos rios da Babilônia nos assentamos");
-		System.out.println("Lembrando de Sião nós choramos, nós choramos");
-		System.out.println("Lá nos salgueiros nós penduramos todas as nossas harpas");
-		System.out.println("Pois nossos opressores a nós pediam canções alegres");
-		System.out.println("De Sião, pois queriam ouvir os cantos");
-		System.out.println("Mas em terra de estranhos era impossível");
+		System.out.println("Nas margens lá dos rios da Babilônia, nos assentamos.");
+		System.out.println("Lembrando de Sião, nós choramos, nós choramos.");
+		System.out.println("Lá nos salgueiros nós penduramos todas as nossas harpas.");
+		System.out.println("Pois, nossos opressores a nós pediam canções alegres.");
+		System.out.println("De Sião, pois queriam ouvir os cantos.");
+		System.out.println("Mas, em terra de estranhos era_impossível");
 		System.out.println("Que minha mão direita resseque toda");
-		System.out.println("Se de Jerusalém_eu vier esquecer");
-		System.out.println("Se de Jerusalém_eu vier esquecer");
+		System.out.println("Se de Jerusalém eu vi'er esquecer,");
+		System.out.println("Se de Jerusalém eu vi'er esquecer.");
 		System.out.println("");
-		System.out.println("Se_eu não lembrar de ti que minha língua ao meu paladar se apegue");
-		System.out.println("Se_eu a Jerusalém não preferir ao regozijo");
-		System.out.println("Dos filhos de Edom lembra-te Senhor para seres contra");
-		System.out.println("Pois de Sião, diziam até as bases arrasai-a");
-		System.out.println("Hás de ser Babilônia, aniquilada");
+		System.out.println("Se_eu não lembrar de ti, que mi'nha língua_ao meu paladar se_apegue,");
+		System.out.println("Se_eu a Jerusalém não preferir ao regozijo.");
+		System.out.println("Dos filhos de Edom lembra-te Senhor para seres contra.");
+		System.out.println("Pois, de Sião diziam: “até as bases arrasai-a.”");
+		System.out.println("Hás de ser, Babilônia, aniquilada.");
 		System.out.println("Quem te der, pois, o pago será bendito");
-		System.out.println("Do mal que nos fizeste, sim grande mal");
+		System.out.println("Do mal que nos fizeste, sim, grande mal.");
 		System.out.println("Feliz quem aos teus filhos pegar, prendê-los");
-		System.out.println("Para esmagá-los todos contra os rochedos");
+		System.out.println("Para_esmagá-los todos contra_os rochedos.");
 	}
 }
