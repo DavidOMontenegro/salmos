6a5ea6b070ace91a8e343b099cde72293884f329
David Montenegro <davidom.official@gmail.com>
2023-04-20 11:49:09 -0300

    add salmo70

---
+++ java/Salmo70.java
@@ -0,0 +1,22 @@
+public class Salmo70 {
+
+	public static void main(String[] args) {
+		System.out.println("Praza-te,_ó Deus, em me livrar!");
+		System.out.println("Sê pressuroso_em me_acudir!");
+		System.out.println("Tenha vergonha_e confusão");
+		System.out.println("Quem minha vida quer tirar.");
+		System.out.println("Envergonhado, volte_atrás");
+		System.out.println("Quem se compraz no meu sofrer.");
+		System.out.println("Vergonha_os faça recuar,");
+		System.out.println("Pois eles zombam do meu mal.");
+		System.out.println("");
+		System.out.println("Quem te procura, ó SENHOR,");
+		System.out.println("Folgue_e jubile sempre_em Ti.");
+		System.out.println("“Magnificado seja Deus!”,");
+		System.out.println("Diga quem ama_a salvação.");
+		System.out.println("Necessitado_e pobre sou.");
+		System.out.println("Ó Deus, te_apressa_em me valer!");
+		System.out.println("Amparo meu, Libertador,");
+		System.out.println("Não te detenhas, ó SENHOR.");
+	}
+}
