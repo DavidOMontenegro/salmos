ac2cf00ae04d41b9aa35e739fe962abcddd7fd51
David Montenegro <davidom.official@gmail.com>
2023-04-20 10:01:49 -0300

    add salmo119

---
+++ java/Salmo119.java
@@ -0,0 +1,29 @@
+public class Salmo119 {
+    
+    public static void main(String[] args) {
+		System.out.println("De que maneira_o jovem poderá");
+		System.out.println("Guardar em santidade_o seu andar?");
+		System.out.println("Observando todo_o caminhar");
+		System.out.println("Conforme_a tua santa lei mandar.");
+		System.out.println("");
+		System.out.println("De todo_o coração eu te busquei,");
+		System.out.println("Não me permitas desprezar a lei.");
+		System.out.println("No coração tua lei guardada está");
+		System.out.println("E contra Ti não venha eu pecar");
+		System.out.println("");
+		System.out.println("Bendito_és Tu, me_ensina, ó SENHOR,");
+		System.out.println("Os teus preceitos, tuas santas leis.");
+		System.out.println("Quero narrar com_os lábios tua lei,");
+		System.out.println("Tudo que tua boca nos falou.");
+		System.out.println("");
+		System.out.println("Mais regozijo tenho_em caminhar");
+		System.out.println("Nos testemunhos que provém de Ti.");
+		System.out.println("Me_alegrarei na tua lei bem mais");
+		System.out.println("Do que em ricas possessões, sem fim.");
+		System.out.println("");
+		System.out.println("Nos teus preceitos eu meditarei,");
+		System.out.println("Respeito_aos teus caminhos manterei,");
+		System.out.println("Nos teus decretos me contentarei,");
+		System.out.println("Tua Palavra não esquecerei.");
+	}
+}
