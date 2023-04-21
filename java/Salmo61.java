b14642a82aa1565a47576f33d836395172a91414
David Montenegro <davidom.official@gmail.com>
2023-04-21 09:12:07 -0300

    add salmo61

---
+++ java/Salmo61.java
@@ -0,0 +1,31 @@
+public class Salmo61 {
+
+	public static void main(String[] args) {
+		System.out.println("Minha_oração atende, ó Deus;");
+		System.out.println("Ouve, ó Pai, meu suplicar.");
+		System.out.println("Dos fins da terra clamo por Ti");
+		System.out.println("Com abatido coração.");
+		System.out.println("Leva-me_à rocha, ó Senhor,");
+		System.out.println("Que_é bem mais alta do que eu.");
+		System.out.println("Pois, torre forte és para mim;");
+		System.out.println("Refúgio contra o homem mau.");
+		System.out.println("");
+		System.out.println("No templo sempre_assista eu;");
+		System.out.println("Sob tu'as asas firme_estou");
+		System.out.println("No_esconderijo protetor;");
+		System.out.println("Pois, escutaste_os votos meus.");
+		System.out.println("Me deste_a_herança dos que tem");
+		System.out.println("Temor ao nome Teu, Senhor.");
+		System.out.println("E longo tempo dás ao rei;");
+		System.out.println("Seus anos duram gerações.");
+		System.out.println("");
+		System.out.println("Que permaneça di'ante de Deus");
+		System.out.println("Para sempre_o nosso rei.");
+		System.out.println("Tua verdade_e Teu favor");
+		System.out.println("Seu reino possam preservar.");
+		System.out.println("Assim, teu nome louvarei;");
+		System.out.println("Com salmos sempre cantarei;");
+		System.out.println("E dia_a dia cumprirei");
+		System.out.println("Os votos feitos ao SENHOR.");
+	}
+}
