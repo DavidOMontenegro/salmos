5463ea5ebc4aa5024a3b89f1767f695bb1ee417b
David Montenegro <davidom.official@gmail.com>
2023-04-21 09:33:43 -0300

    add salmo131

---
+++ java/Salmo131.java
@@ -0,0 +1,19 @@
+public class Salmo131 {
+
+	public static void main(String[] args) {
+		System.out.println("Não é soberbo_o meu coração");
+		System.out.println("Nem tenho_altivo_o_olhar;");
+		System.out.println("As grandes coisas não vou buscar,");
+		System.out.println("Grandes demais pra mim.");
+		System.out.println("");
+		System.out.println("Mas a minh’alma_eu fiz sossegar;");
+		System.out.println("Como_um infante_está,");
+		System.out.println("Bem desmamado, com sua mãe,");
+		System.out.println("Minh’alma_assim está.");
+		System.out.println("");
+		System.out.println("Ó Israel, espera em Deus,");
+		System.out.println("Espera no Senhor");
+		System.out.println("Desde_o presente, que_agora é");
+		System.out.println("E para sempre._Amém.");
+	}
+}
