ca327bab1002c6d7819d5c2bb8aefedefdc8c8fc
David Montenegro <davidom.official@gmail.com>
2023-04-21 09:39:20 -0300

    add salmo117

---
+++ java/Salmo117.java
@@ -0,0 +1,13 @@
+public class Salmo117 {
+
+	public static void main(String[] args) {
+		System.out.println("Ao Senhor Deus rendei louvor;");
+		System.out.println("Todos os povos, adorai;");
+		System.out.println("Dai louvores, ó gentios!");
+		System.out.println("Porque mui grande_é_o Seu favor,");
+		System.out.println("Sua misericórdia_em nós.");
+		System.out.println("Do Senhor, fidelidade");
+		System.out.println("Para sempre permanece!");
+		System.out.println("Aleluia!");
+	}
+}
