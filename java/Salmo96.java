e7f2dfdd99f91a2a561f17b226ba2812a36a8c75
David Montenegro <davidom.official@gmail.com>
2023-04-24 15:40:44 -0300

    add salmo96

---
+++ java/Salmo96.java
@@ -0,0 +1,30 @@
+public class Salmo96 {
+
+	public static void main(String[] args) {
+		System.out.println("Cantai ao Senhor um cântico novo.");
+		System.out.println("Cantai ao Senhor, todas as terras.");
+		System.out.println("Cantai ao Senhor, bendizei o seu nome,");
+		System.out.println("Proclamai a sua salvação.");
+		System.out.println("Anunciai entre_as nações a sua glória;");
+		System.out.println("Entre todos os povos, as suas maravilhas;");
+		System.out.println("Porque grande_é o Senhor e mui digno de ser louvado,");
+		System.out.println("Mais temível do que falsos deuses.");
+		System.out.println("Porque_os outros deuses são ídolos apenas,");
+		System.out.println("Porém os céus pelo Senhor foram feitos.");
+		System.out.println("Glória_e majestade estão diante dele,");
+		System.out.println("Força_e formosura no seu santuário.");
+		System.out.println("");
+		System.out.println("Rendei ao Senhor, ó povos todos.");
+		System.out.println("Rendei ao Senhor glória e força.");
+		System.out.println("Rendei ao Senhor o_que_é devido_ao seu nome.");
+		System.out.println("Entrai nos seus átrios com ofertas.");
+		System.out.println("Adorai Sua santidade porque é bela;");
+		System.out.println("Tremei diante dele, povos de toda terra.");
+		System.out.println("Dizei: “Reina o Senhor.” Firmou_o mundo p'ra que não se abale.");
+		System.out.println("Julga_os povos todos com_equidade.");
+		System.out.println("Exultem terra e céus; ruja_o mar e_o que há nele;");
+		System.out.println("Folgue_o campo_e todas as plantas do bosque;");
+		System.out.println("Diante do Senhor porque vem julgar a terra");
+		System.out.println("Julga_o mundo_e_os povos fiel e justamente.");
+	}
+}
