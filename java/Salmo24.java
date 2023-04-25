7061a8e53407024441aacae3a4d6797695ac9f46
David Montenegro <davidom.official@gmail.com>
2023-04-25 08:42:35 -0300

    add salmo24

---
+++ java/Salmo24.java
@@ -0,0 +1,34 @@
+public class Salmo24 {
+
+	public static void main(String[] args) {
+		System.out.println("Do SENHOR é_a terra e_a sua plenitude,");
+		System.out.println("O mundo e todos os habitantes seus,");
+		System.out.println("Pois sobre os mares Ele_a fundamentou");
+		System.out.println("E sobre_as correntes, estabeleceu.");
+		System.out.println("");
+		System.out.println("Quem, pois, subirá para_o monte do SENHOR");
+		System.out.println("Ou quem estará no seu santo lugar?");
+		System.out.println("Aquele que_é limpo de mãos e coração,");
+		System.out.println("Não ama vaidade,_enganoso não é.");
+		System.out.println("");
+		System.out.println("Tal homem recebe a bênção do SENHOR,");
+		System.out.println("Justiça do Deus da sua salvação,");
+		System.out.println("Tal é a linhagem daqueles que o buscam,");
+		System.out.println("Que buscam a face do Deus de Jacó.");
+		System.out.println("");
+		System.out.println("Erguei já, ó portas, as vossas cabeças;");
+		System.out.println("Portais eternais, levantais-vos ao Rei!");
+		System.out.println("A fim de que entre ali o Rei da Glória,");
+		System.out.println("Portais eternais, levantais-vos ao Rei!");
+		System.out.println("");
+		System.out.println("Quem é_o Rei da Glória,_este Rei tão glorioso?");
+		System.out.println("SENHOR das batalhas que_é forte_em poder.");
+		System.out.println("Erguei já, ó portas, as vossas cabeças;");
+		System.out.println("Portais eternais, levantais-vos ao Rei!");
+		System.out.println("");
+		System.out.println("A fim de que entre ali o Rei da Glória,");
+		System.out.println("Portais eternais, levantais-vos ao Rei!");
+		System.out.println("Quem é_o Rei da Glória,_este Rei tão glorioso?");
+		System.out.println("Ele_é_o Rei da Glória, das hostes Senhor!");
+	}
+}
