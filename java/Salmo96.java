f6ca971ead9eb4d4edce475130b3da5e5e3beb28
David Montenegro <davidom.official@gmail.com>
2023-04-28 08:52:47 -0300

    fix salmo96

--- java/Salmo96.java
+++ java/Salmo96.java
@@ -5,7 +5,7 @@ public class Salmo96 {
 		System.out.println("Cantai ao Senhor, todas as terras.");
 		System.out.println("Cantai ao Senhor, bendizei o seu nome,");
 		System.out.println("Proclamai a sua salvação.");
-		System.out.println("Anunciai entre_as nações a sua glória;");
+		System.out.println("Anunci'ai entre_as nações a sua glória;");
 		System.out.println("Entre todos os povos, as suas maravilhas;");
 		System.out.println("Porque grande_é o Senhor e mui digno de ser louvado,");
 		System.out.println("Mais temível do que falsos deuses.");
@@ -18,13 +18,13 @@ public class Salmo96 {
 		System.out.println("Rendei ao Senhor glória e força.");
 		System.out.println("Rendei ao Senhor o_que_é devido_ao seu nome.");
 		System.out.println("Entrai nos seus átrios com ofertas.");
-		System.out.println("Adorai Sua santidade porque é bela;");
+		System.out.println("Adorai Su'a santidade porque é bela;");
 		System.out.println("Tremei diante dele, povos de toda terra.");
 		System.out.println("Dizei: “Reina o Senhor.” Firmou_o mundo p'ra que não se_abale.");
 		System.out.println("Julga_os povos todos com_equidade.");
-		System.out.println("Exultem terra e céus; ruja_o mar e_o que há nele;");
-		System.out.println("Regozije o campo e sua plenitude;");
-		System.out.println("Diante do Senhor porque vem julgar a terra");
+		System.out.println("Exultem terra_e céus; ruja_o mar e_o que há nele;");
+		System.out.println("Regozije_o campo e su'a plenitude;");
+		System.out.println("Di'ante do Senhor porque vem julgar a terra,");
 		System.out.println("Julgará o mundo fiel_e justamente.");
 	}
 }
